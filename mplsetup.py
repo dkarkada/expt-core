5ec5a14b74be98bb9224bcb884c76fa70e8da28c
dhruva <dkarkada@gmail.com>
2024-01-19 13:44:15 -0800

    rcsetup for colab

--- mplsetup.py
+++ mplsetup.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
 
-def rcsetup():
-    plt.rc("figure", dpi=150, facecolor=(1, 1, 1))
-    plt.rc("font", family='palatino linotype', size=14)
-    plt.rc("axes", facecolor=(1, .99, .95), titlesize=18)
+def rcsetup(colab=True):
+    if colab:
+        plt.rc("figure", dpi=120, facecolor=(1, 1, 1))
+        plt.rc("font", family='stixgeneral', size=12)
+        plt.rc("axes", facecolor=(1, .99, .95), titlesize=12)
+    else:
+        plt.rc("figure", dpi=150, facecolor=(1, 1, 1))
+        plt.rc("font", family='palatino linotype', size=14)
+        plt.rc("axes", facecolor=(1, .99, .95), titlesize=18)
     plt.rc("mathtext", fontset='cm')
 
 def make_plot(ax, title, xlabel, ylabel, drawax=True, drawgrid=True, ylim=None, xlim=None):    
