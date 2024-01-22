00c3d1d0f216d56e74ef312860dbd1fa0112d76b
Dhruva <dkarkada@gmail.com>
2024-01-22 13:15:43 -0800

    fixed filepath stuff

--- FileManager.py
+++ FileManager.py
@@ -12,13 +12,12 @@ class FileManager():
         self.filepath = os.path.join(self.root, filepath)
         os.makedirs(self.filepath, exist_ok=True)
     
-    def get_filename(self, fn, filepath=None):
-        if filepath:
-            self.set_filepath(filepath)
+    def get_filename(self, fn, filepath=''):
+        self.set_filepath(filepath)
         fn = os.path.join(self.filepath, fn)
         return fn
 
-    def save(self, obj, fn, filepath=None):
+    def save(self, obj, fn, filepath=''):
         fn = self.get_filename(fn, filepath)
         if fn.endswith('.npy'):
             assert isinstance(obj, np.ndarray)
@@ -27,7 +26,7 @@ class FileManager():
         with open(fn, 'wb') as handle:
             pickle.dump(obj, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    def load(self, fn, filepath=None):
+    def load(self, fn, filepath=''):
         fn = self.get_filename(fn, filepath)
         if not os.path.isfile(fn):
             return None
